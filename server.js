354cfec59935399a2c4238f2a5695491d00245f0
VladimriFilim <boterfly9@gmail.com>
2024-05-23 20:58:01 +0400

    added reading element from document by id

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getDb, connectToDb} = require('./db')
+const { connectToDb, getDb } = require('./db');
 
 const PORT = 3000;
 
@@ -8,12 +8,51 @@ const app = express();
 let db;
 
 connectToDb((err) => {
-   if(!err) {
-       app.listen(PORT, (err) => {
-           err ? console.log(err) : console.log(`Listening port ${PORT}`);
-       });
-       db = getDb();
-   } else {
-       console.log(`DB connection error: ${err}`)
-   }
+    if (!err) {
+        app.listen(PORT, (err) => {
+            err ? console.log(err) : console.log(`listening port ${PORT}`);
+        });
+        db = getDb();
+    } else {
+        console.log(`DB connection error: ${err}`);
+    }
+});
+
+const handleError = (res, error) => {
+    res.status(500).json({ error });
+}
+
+app.get('/movies', (req, res) => {
+    const movies = [];
+
+    db
+        .collection('movies')
+        .find()
+        .sort({ title: 1 })
+        .forEach((movie) => movies.push(movie))
+        .then(() => {
+            res
+                .status(200)
+                .json(movies);
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Something goes wrong..." })
+        })
+});
+app.get('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db
+            .collection('movies')
+            .findOne({ _id: ObjectId(req.params.id) })
+            .then((doc) => {
+                res
+                    .status(200)
+                    .json(doc);
+            })
+            .catch(() => handleError(res, "Something goes wrong..."));
+    } else {
+        handleError(res, "Wrong id");
+    }
 });
